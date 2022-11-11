d8ea606532d2aa5c5e42ff3a0e25c532d4c6c99c
skf <2119104071@qq.com>
2022-11-11 16:37:33 +0800

    complete mysql_t.py

---
+++ mysql_t.py
@@ -0,0 +1,67 @@
+import pymysql
+
+def create_database():
+	db = pymysql.connect(host='localhost', user='skf', password='123456')
+	cursor = db.cursor()
+	cursor.execute('SELECT VERSION()')
+	data = cursor.fetchone()
+	print('Database version:', data)
+	cursor.execute("CREATE DATABASE IF NOT EXISTS spiders DEFAULT CHARACTER SET utf8mb4")
+	db.close()
+
+def insert_into(data:'dict', table:'str'):
+	db = pymysql.connect(host='localhost', user='skf', password='123456')
+	cursor = db.cursor()
+	keys = ', '.join(data.keys())
+	values = ', '.join(['%s'] * len(data))
+	sql = f'INSERT INTO {table}({keys}) VALUES ({values})'
+	try :
+		if cursor.excute(sql, tuple(data.values())):
+			print('Successful')
+			db.commit()
+	except:
+		print('Failed')
+		db.rollback()
+	db.close()
+
+def insert_or_update(data:'dict', table:'str'):
+	db = pymysql.connect(host='localhost', user='skf', password='123456')
+	cursor = db.cursor()
+	keys = ', '.join(data.keys())
+	values = ', '.join(['%s'] * len(data))
+	sql = f'INSERT INTO {table}({keys}) VALUES ({values}) ON DUPLICATE KEY UPDATE'
+	update = ', '.join([f"{key} = %ss" for key in data])
+	sql += update
+	try:
+		if cursor.excute(sql, tuple(data.values()) * 2):
+			print('Successful')
+			db.commit()
+	except:
+		print('Failed')
+		db.rollback()
+	db.close()
+	
+def delete(table, condition):
+	db = pymysql.connect(host='localhost', user='skf', password='123456')
+	cursor = db.cursor()
+	sql = f'DELETE FROM {table} WHERE {condition}'
+	try:
+		cursor.excute(sql)
+		db.commit()
+	except:
+		db.rollback()
+	db.close()
+
+def select_obo(sql):
+	try:
+		db = pymysql.connect(host='localhost', user='skf', password='123456')
+		cursor = db.cursor()
+		cursor.excute(sql)
+		print('Count:', cursor.rowcount)
+		row = cursor.fetchone()
+		while row:
+			print('Row:', row)
+			row = cursor.fetchone()
+	except:
+		print('Error')
+		
--- t.py
+++ t.py
@@ -1,10 +1,1 @@
-import pymysql
 
-db = pymysql.connect(host='localhost', user='skf', password='123456')
-cursor = db.cursor()
-cursor.execute('SELECT VERSION()')
-data = cursor.fetchone()
-print('Database version:', data)
-cursor.execute("CREATE DATABASE spiders DEFAULT CHARACTER SET utf8mb4")
-db.close()
-		
