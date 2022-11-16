1aa151c679fa63a2248f9f28e90b2b5df061d61d
skf <2119104071@qq.com>
2022-11-16 22:31:24 +0800

    test

--- t.py
+++ t.py
@@ -1,8 +1,1 @@
-import asyncio
-import aiomysql
-import sqlalchemy
-import logging
-
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s: %(message)s')
 
