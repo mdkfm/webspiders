e612d292a7ee9658aa4fbd9cf48be4c7082c3a82
skf <2119104071@qq.com>
2022-11-16 15:21:53 +0800

    test

--- selenium_t.py
+++ selenium_t.py
@@ -3,17 +3,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
 
-browser = webdriver.Chrome()
-try:
-    browser.get('https://www.baidu.com')
-    input = browser.find_element_by_id('kw')
-    input.send_keys('Python')
-    input.send_keys(Keys.ENTER)
-    wait = WebDriverWait(browser, 10)
-    wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
-    print(browser.current_url)
-    print(browser.get_cookies())
-    print(browser.page_source)
-finally:
-    browser.close()
+import time
+
+browser = webdriver.Firefox()
+
+def find_t():
+	browser.get('https://www.taobao.com')
+	input1 = browser.find_element(By.ID, 'q')
+	input2 = browser.find_element('css selector', '#q')
+	input3 = browser.find_element('xpath', '//*[@id="q"]')
+	#find_element函数的第一个参数可以为字符串或者By类，用来指定搜寻依据
+	print(input1, input2, input3)
+	browser.close()
+	
+def interact_t():
+	browser.get('https://www.taobao.com')
+	input = browser.find_element(By.ID, 'q')
+	input.send_keys('iPhone')
+	time.sleep(1)
+	input.clear()
+	input.send_keys('iPad')
+	button = browser.find_element('css selector', '.btn-search')
+	button.click()
+	browser.close()
+
+def actions_t():
+	url = 'https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable'
+	browser.get(url)
+	browser.switch_to.frame('iframeResult')
+	source = browser.find_element('css selector', '#draggable')
+	target = browser.find_element('css selector', '#droppable')
+	actions = ActionChains(browser)
+	actions.drag_and_drop(source, target)
+	actions.perform()
+
+actions_t()
+
