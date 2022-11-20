c3c81dbbf06802a9e7c20465709235075ca3a719
skf <2119104071@qq.com>
2022-11-20 13:04:50 +0800

    test

--- playwright_spa2.py
+++ playwright_spa2.py
@@ -1,46 +1,42 @@
 import asyncio
 from playwright.async_api import async_playwright
 import aiomysql
-from pyquery import PyQuery as pq
 import logging
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
 
 INDEX_URL = 'https://spa2.scrape.center/page/{page}'
-DETAIL_URL = 'https://spa2.scrape.center/detail/{href}'
+DETAIL_URL = 'https://spa2.scrape.center{href}'
 TIMEOUT = 10
 TOTAL_PAGE = 10
-CONCURRENCY = 5
-semaphore = asyncio.Semaphore(CONCURRENCY)
+# ~ CONCURRENCY = 5
+# ~ semaphore = asyncio.Semaphore(CONCURRENCY)
 
 async def on_response(response):
 	if '/api/movie/' in response.url and response.status == 200:
 		return await response.json()
 	
-
 async def scrape_detail(href, browser):
-	async with browser.new_context() as context:
+	async with await browser.new_context() as context:
 		url = DETAIL_URL.format(href=href)
 		page = await context.new_page()
 		await page.on('response', on_response)
 		await page.goto(url)
-		
+		logging.info('scrape detail %s', url)
 
 async def scrape_index(page, browser):
-	async with browser.new_context() as context:
+	async with await browser.new_context() as context:
 		url =  INDEX_URL.format(page=page)
 		page = await context.new_page()
 		await page.goto(url)
+		await page.wait_for_load_state(state='networkidle')
+		elements = page.query_selector_all('a.name')
 		logging.info('scrape index %s', url)
-		html = await page.content()
-		doc = pq(html)
-		items = doc('.item .name')
-		herfs = []
-		for item in items:
-			herfs.append(item.attr('herf'))
-		return herfs
-		
+		hrefs = []
+		for element in elements:
+			hrefs.append(element.get_attribute('href'))
+		return hrefs
 
 async def main_scrape(page, browser):
 	hrefs = await scrape_index(page, browser)
